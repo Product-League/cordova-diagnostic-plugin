20899493054a5bec8f77c7a3ef28d917b6120f91
Paulo Solipa <psolipa@product-league.com>
2025-04-03 21:57:55 +0100

    Test 1.2 to remove permissions on Android

---
+++ scripts/remove_permissions.js
@@ -0,0 +1,26 @@
+var permissionsToRemove = [ "READ_MEDIA_IMAGES", "READ_MEDIA_VIDEO"];
+
+const fs = require('fs');
+const path = require('path');
+const rootdir = "";
+const manifestFile = path.join(rootdir, "platforms/android/app/src/main/AndroidManifest.xml");
+
+fs.readFile( manifestFile, "utf8", function( err, data )
+{
+    if (err)
+        return console.log( err );
+
+    let result = data;
+    for (var i=0; i<permissionsToRemove.length; i++){
+        const regex = new RegExp(`<uses-permission[^>]*android:name=["']${permissionsToRemove[i]}["'][^>]*/?>`, 'gi');
+        if (regex.test(manifestContent)) {
+            result = result.replace(regex, '');
+        }
+    }
+
+    fs.writeFile( manifestFile, result, "utf8", function( err )
+    {
+        if (err)
+            return console.log( err );
+    } );
+} );
